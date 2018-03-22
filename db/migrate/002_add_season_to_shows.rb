67dccb02cfe8022bf79a21f9f6482d98324f9b49
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-22 00:26:37 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
-class AddSeasonToShow < ActiveRecord::Migration 
-  def change 
+class AddSeasonToShow < ActiveRecord::Migration
+  def change
     add_column(:students, :season, :string)
-  end 
+  end
 end 
