d22379bc962a10d985ba2881f57d72329b199258
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-22 00:35:53 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,7 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.order(rating: :desc).first.rating
 end
+
+  def self.most_popular_show
+    self.order(rating: :desc).first.title
 end
