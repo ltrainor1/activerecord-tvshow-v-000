24d0c5c94d996bb3d89ef6c740ea406be4fc532d
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-22 00:37:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,13 @@ end
   def self.most_popular_show
     self.order(rating: :desc).first.name
 end
+
+def self.lowest_rating
+  self.order(rating: :asc).first.rating
+end 
+
+def self.least_popular_show
+  self.order(rating: :asc).first.name 
+end 
+
 end
